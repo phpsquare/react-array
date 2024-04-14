44b6eaf096898b5fe69dc4a97adaab379c42d55b
Shantanu Mukherjee <phpsquare@gmail.com>
2024-04-14 19:36:03 +0530

    first upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+
+function ArrayList()
+{
+	const products = [
+	
+	{title:'p1',desc:'d1'},
+	{title:'p2',desc:'d2'},
+	{title:'p3',desc:'d3'},
+	{title:'p4',desc:'d4',},
+	
+	];
+	
+	const listProduct = products.map((item) => {
+		return(<p>{item.title} {item.desc}</p>);
+	});
+	
+	return <div>{listProduct}</div>;
+		
+}
+
+	
+
+
 
 function App() {
   return (
@@ -9,15 +33,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div><ArrayList /></div>
       </header>
+	
     </div>
   );
 }
